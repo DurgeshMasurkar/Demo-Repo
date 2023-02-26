e25d1a60910cdad89aa021249da958cc1783dd9a
Durgesh  Masurkar <79824666+DurgeshMasurkar@users.noreply.github.com>
2023-02-26 11:36:55 +0530

    Add files via upload

---
+++ AbractionDemo.java
@@ -0,0 +1,39 @@
+package basicoops;
+
+abstract class CommonFeatures{
+	void login() {
+		System.out.println("User Logged In");
+	}
+	abstract void ContentAccess();
+}
+
+class FreeUser extends CommonFeatures{
+
+	@Override
+	void ContentAccess() {
+		System.out.println("Limited IPL with ADS");
+	}
+	
+}
+class VipUser extends CommonFeatures{
+
+	@Override
+	void ContentAccess() {
+		System.out.println("Full IPL with ADS ");
+	}
+	void Payment() {
+		System.out.println("Payment Method");
+	}
+	
+}
+
+public class AbractionDemo {
+
+	public static void main(String[] args) {
+		CommonFeatures v=new VipUser();
+		v.login();
+		
+
+	}
+
+}
